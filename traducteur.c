4b422553e6b4cc86109ab46d319166f8d0c0b9eb
Lucazette21 <81782352+Lucazette21@users.noreply.github.com>
2024-06-01 15:16:13 +0200

    Update traducteur.c

--- traducteur.c
+++ traducteur.c
@@ -5,21 +5,37 @@
 #include <assert.h>
 #include "lexer.h" 
 
-
+/*gere les operateur dans les variable*/
 maillon* contenu_variable(maillon* debut){
-
+    maillon* contenu = debut;
+    while ( contenu->argument != ";" ) {
+        assert(contenu->argument != NULL);
+        if (contenu->argument == "%") {
+            printf("mod");
+        }
+        else {printf("%s", contenu->argument )};
+        contenu = contenu->suivant ;
+    }
+    return contenu;
 }
-
+/*traduit les creation variable*/
 maillon* variable(maillon* debut){
     assert(debut->lexeme == "T" || debut->lexeme != "void");
     printf("let ");
+
     maillon* var = debut->suivant;
     maillon* egal = var->suivant;
-    maillon* contenu = egal->suivant;
-    printf("%s = ref ", var->argument);
-
+    maillon* contenu = egal->suivant;      
+    printf("%s = ref" , var->argument);
+    contenu = contenu_variable(contenu);
+    return contenu;
 }
 
+/*traduit les redefinition de variable*/
+maillon* redef_variable(maillon* debut){
+
+}
+/*fonction générale*/
 void traduction(maillon* debut){
     maillon* charactere = debut;
     while (charactere->argument != "{"){
